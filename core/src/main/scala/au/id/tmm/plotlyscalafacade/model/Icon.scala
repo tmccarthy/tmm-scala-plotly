162fcb44c068c4f390a79cbcc7e82f3da547c7fa
Timothy McCarthy <ebh042@gmail.com>
2020-07-14 12:52:25 +1000

    More

--- core/src/main/scala/au/id/tmm/plotlyscalafacade/model/Icon.scala
+++ core/src/main/scala/au/id/tmm/plotlyscalafacade/model/Icon.scala
@@ -1,5 +1,7 @@
 package au.id.tmm.plotlyscalafacade.model
 
+import io.circe.Encoder
+
 final case class Icon(
   height: Option[Number],
   width: Option[Number],
@@ -10,3 +12,27 @@ final case class Icon(
   svg: Option[String],
   transform: Option[String],
 )
+
+object Icon {
+  implicit val encoder: Encoder[Icon] = Encoder.forProduct8(
+    "height",
+    "width",
+    "ascent",
+    "descent",
+    "name",
+    "path",
+    "svg",
+    "transform",
+  )(i =>
+    (
+      i.height,
+      i.width,
+      i.ascent,
+      i.descent,
+      i.name,
+      i.path,
+      i.svg,
+      i.transform,
+    ),
+  )
+}
---
+++ core/src/main/scala/au/id/tmm/plotlyscalafacade/model/ModeBarButton.scala
@@ -0,0 +1,30 @@
+package au.id.tmm.plotlyscalafacade.model
+
+import au.id.tmm.plotlyscalafacade.model
+import io.circe.Encoder
+
+final case class ModeBarButton(
+  name: String,
+  title: String,
+  icon: ModeBarButton.Icon,
+  gravity: Option[String],
+  attr: Option[String],
+  `val`: Option[String],
+  toggle: Option[Boolean],
+)
+
+object ModeBarButton {
+
+  sealed trait Icon
+
+  object Icon {
+    final case class OfString(asString: String) extends ModeBarButton.Icon
+    final case class OfIcon(icon: model.Icon)   extends ModeBarButton.Icon
+
+    implicit val encoder: Encoder[ModeBarButton.Icon] = {
+      case OfString(asString) => Encoder[String].apply(asString)
+      case OfIcon(icon) => Encoder[model.Icon].apply(icon)
+    }
+  }
+
+}
